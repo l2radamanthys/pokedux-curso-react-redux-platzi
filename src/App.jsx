501f37f77d8ab15954dd21d6b5dd383e0957dde6
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-09-26 12:53:48 -0300

    instalado antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { Button } from "antd";
 
 function App() {
-  return <>Pokedux</>;
+  return (
+    <>
+      Pokedux
+      <Button type="primary">Boton</Button>
+    </>
+  );
 }
 
 export default App;
